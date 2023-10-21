16a741e050b89a5b89d1e74883df853c3ab05792
Kamrul Hasan <991khriad@gmail.com>
2023-10-22 00:12:50 +0600

    1st commit for git

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+         <h1>this is From App.js file</h1>
+         <strong>This is only for dem test </strong><br/><br/>
+         <span>Author : Kamrul Hasan</span>
+     </div>
+    </div>
+  );
+}
+
+export default App;
